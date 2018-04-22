ba71eb4b49add775c822364e55bdf72d117be81d
aoitaku <aoitaku@gmail.com>
2018-04-23 00:59:15 +0900

    chore: ssh-add をシェルスクリプトでなく .travis.yml で行うように

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-eval "$(ssh-agent -s)"
-chmod 600 ~/.ssh/id_rsa
-ssh-add ~/.ssh/id_rsa
-
 git config --global push.default matching
 git remote add deploy ssh://git@$IP:$PORT$DEPLOY_DIR
 git push deploy master
