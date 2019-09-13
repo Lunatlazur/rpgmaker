efc1f38f8a184bdb227d7402e01b4251b84dc682
aoitaku <aoitaku@gmail.com>
2019-09-14 02:54:33 +0900

    feat: wip メッセージの追加フォントを選択肢に反映

--- packages/ConfigurableMessageWindow/src/Lunatlazur_ConfigurableMessageWindow.ts
+++ packages/ConfigurableMessageWindow/src/Lunatlazur_ConfigurableMessageWindow.ts
@@ -18,13 +18,23 @@
  * @help This plugin makes the message window configurable.
  *
  * @param Number of message rows
- * @desc number of message rows.
+ * @desc Number of message rows.
  * @default 4
+ * @type number
+ *
+ * @param Custom Message wait
+ * @desc Change wait time during character feed.
+ * @default 1
  * @type number
  *
  * @param Custom message font
  * @desc Additional message fonts.
  * @type string[]
+ *
+ * @param Apply custom message font to choice
+ * @desc Apply custom message font to choice if true.
+ * @default true
+ * @type boolean
  */
 /*:ja
  * @plugindesc メッセージウィンドウカスタムプラグイン
@@ -45,6 +55,11 @@
  * @desc 追加のフォントを指定できます。先頭にあるものが優先して読み込まれます。
  * @default ["UD デジタル 教科書体 NP-R", "Klee"]
  * @type string[]
+ *
+ * @param 選択肢に追加のフォント指定を反映
+ * @desc 有効にすると選択肢に追加のフォント指定を反映します。
+ * @default true
+ * @type boolean
  */
 (function () {
   const pluginName = 'Lunatlazur_ConfigurableMessageWindow'
@@ -73,20 +88,46 @@
   const parameters = PluginManager.parameters(pluginName)
 
   const params = {
-    numberOfMessageRows: asNumber(parameters, 'メッセージ行数', 'Number of message rows'),
-    customMessageFont: JSON.parse(asString(parameters, 'フォント', 'Custom message font')),
-    customMessageWait: asNumber(parameters, 'メッセージウェイト', 'Message wait'),
+    numberOfMessageRows: asNumber(
+      parameters,
+      'メッセージ行数',
+      'Number of message rows',
+    ),
+    customMessageWait: asNumber(
+      parameters,
+      'メッセージウェイト',
+      'Custom Message wait',
+    ),
+    customMessageFont: JSON.parse(asString(
+      parameters,
+      'フォント',
+      'Custom message font',
+    )),
+    applyCustomMessageFontToChoice: asBoolean(
+      parameters,
+      '選択肢に追加のフォント指定を反映',
+      'Apply custom message font to choice',
+    ),
   }
 
   const _Window_Base_standardFontFace = Window_Base.prototype.standardFontFace
   Window_Message.prototype.standardFontFace = function () {
     const fontfaces = [
-      ...params.customMessageFont, _Window_Base_standardFontFace.call(this),
+      ...params.customMessageFont,
+      _Window_Base_standardFontFace.call(this),
     ].join(',')
     return fontfaces
   }
 
-  const _Window_Message_updateMessage = Window_Message.prototype.updateMessage;
+  Window_ChoiceList.prototype.standardFontFace = function () {
+    const fontfaces = [
+      ...params.customMessageFont,
+      _Window_Base_standardFontFace.call(this),
+    ].join(',')
+    return fontfaces
+  }
+
+  const _Window_Message_updateMessage = Window_Message.prototype.updateMessage
   Window_Message.prototype.updateMessage = function() {
     const speed = _Window_Message_getMessageSpeed()
     if (this._textState && !this._lineShowFast && !this._showFast) {
@@ -105,6 +146,7 @@
 
   const _Window_Message_numVisibleRows = Window_Message.prototype.numVisibleRows
   Window_Message.prototype.numVisibleRows = function() {
-    return params.numberOfMessageRows || _Window_Message_numVisibleRows.call(this)
+    return params.numberOfMessageRows ||
+      _Window_Message_numVisibleRows.call(this)
   }
 })()
