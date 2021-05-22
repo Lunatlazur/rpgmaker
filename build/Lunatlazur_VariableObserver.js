3c9bb1d39dd32c4a8f9041229e5919f613f6fe68
aoitaku <aoitaku@gmail.com>
2021-05-22 21:26:59 +0900

    refactor: variable-observerプラグインのコードを整理

--- build/Lunatlazur_VariableObserver.js
+++ build/Lunatlazur_VariableObserver.js
@@ -2,30 +2,121 @@
 // Lunatlazur_VariableObserver.js
 // ----------------------------------------------------------------------------
 // Copyright (c) 2018 Taku Aoi
-// This plugin is released under the zlib/libpng License.
-// http://zlib.net/zlib_license.html
+// This plugin is released under the zlib License.
+// https://zlib.net/zlib_license.html
 // ----------------------------------------------------------------------------
 // Version
-// 1.0.0 2018/10/14
+// 1.0.0 2018-10-14
 // ----------------------------------------------------------------------------
-// [Web]    : https://lunatlazur.com/
-// [Twitter]: https://twitter.com/aoitaku/
-// [GitHub] : https://github.com/aoitaku/
+// [Web]    : https://lunatlazur.com
+// [Twitter]: https://twitter.com/aoitaku
+// [GitHub] : https://github.com/lunatlazur
 //=============================================================================
+/*:
+ * @plugindesc Monitor variable changes
+ * @author Taku Aoi
+ * @help This plugin provides the feature to perform the specified action
+ * when the value of a variable changes.
+ *
+ * Usage
+ * =====
+ *
+ * Plug-in command list
+ * ====================
+ *
+ * ACTIVATE_VARIABLE_OBSERVER
+ * --------------------------
+ *
+ *     ACTIVATE_VARIABLE_OBSERVER <target variable number> <common event id>
+ *
+ * Start monitoring variables.
+ *
+ *     <target variable number>
+ *
+ *
+ * Specifies the number of the variable to be monitored.
+ *
+ *
+ *     <common event id>
+ *
+ * Specifies the ID of the common event that will be executed when the variable
+ * changes.
+ *
+ *
+ * DEACTIVATE_VARIABLE_OBSERVER
+ * --------------------------
+ *
+ *     DEACTIVATE_VARIABLE_OBSERVER <target variable number>
+ *
+ * Stop monitoring variables.
+ *
+ *
+ *     <target variable number>
+ *
+ * Specifies the number of the variable to stop monitoring.
+ *
+ *
+ * Changelog
+ * =========
+ *
+ * 1.0.0 2018-10-14
+ * ----------------
+ * - Published.
+ *
+ */
 /*:ja
  * @plugindesc 変数監視プラグイン
  * @author あおいたく
  * @help このプラグインは任意の変数を監視して、値の変更時に行う処理を設定できるようにします。
  *
- * 変更履歴
- * ********
+ * 使い方
+ * ======
  *
- * 1.0.0 2020/01/22:
- *   - 公開
+ * プラグインコマンド一覧
+ * ======================
+ *
+ * 変数監視開始
+ * ----------
+ *
+ *     変数監視開始 <監視する変数番号> <コモンイベントID>
+ *
+ * 変数の監視を開始します。
+ *
+ *
+ *     <監視する変数番号>
+ *
+ * 監視する変数の番号を指定します。
+ *
+ *
+ *     <コモンイベントID>
+ *
+ * 変数に変更があったときに実行されるコモンイベントのIDを指定します。
+ *
+ *
+ * 変数監視停止
+ * ----------
+ *
+ *     変数監視停止 <監視を停止する変数番号>
+ *
+ * 変数の監視を停止します。
+ *
+ *
+ *     <監視を停止する変数番号>
+ *
+ * 監視を停止する変数の番号を指定します。
+ *
+ *
+ * 変更履歴
+ * ========
+ *
+ * 1.0.0 2018-10-14
+ * ----------------
+ * - 公開
  *
  */
 (function () {
-    const pluginName = 'Lunatlazur_VariableObserver';
+    'use strict';
+
     const observedVariables = {};
     function execCommonEvent(commonEventId) {
         const commonEvent = $dataCommonEvents[commonEventId];
@@ -70,4 +161,5 @@
         }).filter(([key, observer]) => observer.previousValue !== $gameVariables.value(key))
             .forEach(([_, observer]) => execCommonEvent(observer.commonEventId));
     };
-})();
+
+}());
---
+++ packages/variable-observer/README.en.md
@@ -0,0 +1,23 @@
+# VariableObserver
+Variable observer plugin for RPG Maker MV
+
+<!-- % screenshot here % -->
+
+---
+
+This plugin provides the feature to perform the specified action
+when the value of a variable changes.
+
+---
+
+# Install
+Copy `Lunatlazur_VariableObserver.js` in `build` forder to `js/plugins` forder in your project.
+
+# Usage
+See [usage](USAGE.en.md).
+
+# Changelog
+See [changelog](CHANGELOG.en.md).
+
+# License
+This plugin is released under the [zlib License](LISENCE.md).
